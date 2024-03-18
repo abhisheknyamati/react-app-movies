4527d3416605e0582be450ff037518865c3a0f37
slangboom <abhisheknyamati@gmail.com>
2024-03-18 21:09:16 +0530

    api link not working and search bar static

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+const API_URL = `http://www.omdbapi.com?apikey=4373d587`;
+
+const movie1 = {
+    "Title": "Spiderman the Verse",
+    "Year": "2019",
+    "imdbID": "tt12122034",
+    "Type": "series",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
+}
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('spiderman');
+    }, []);
+    
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input placeholder="search for movie names"
+                    value="superman"
+                    onChange={() => { }}
+                ></input>
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }}></img>
+            </div>
+
+            {
+                movies?.length > 0 ? (
+                    <div className="container">
+                        {
+                            movies.map((movie) => {
+                                <MovieCard movie={movie} />
+                            })
+                        }
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default App;
