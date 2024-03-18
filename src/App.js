1d81bee2d4827367438f9a915251e91bd4420356
slangboom <abhisheknyamati@gmail.com>
2024-03-19 01:36:43 +0530

    all working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
-const API_URL = `http://www.omdbapi.com?apikey=4373d587`;
-
-const movie1 = {
-    "Title": "Spiderman the Verse",
-    "Year": "2019",
-    "imdbID": "tt12122034",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
-}
+const API_URL = `http://www.omdbapi.com?apikey=88c7a064`;
 
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -33,22 +26,22 @@ const App = () => {
             <h1>MovieLand</h1>
             <div className="search">
                 <input placeholder="search for movie names"
-                    value="superman"
-                    onChange={() => { }}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 ></input>
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => { }}></img>
+                    onClick={() => searchMovies(searchTerm)}></img>
             </div>
 
             {
                 movies?.length > 0 ? (
                     <div className="container">
                         {
-                            movies.map((movie) => {
+                            movies.map((movie) => 
                                 <MovieCard movie={movie} />
-                            })
+                            )
                         }
                     </div>
                 ) : (
